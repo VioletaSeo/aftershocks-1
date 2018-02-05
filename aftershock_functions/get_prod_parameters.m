b61e0cd608b37a3852e11f61dd88a89999293961
keliankaz <kelian.dascher-cousineau@mail.mcgill.ca>
2018-02-05 13:38:56 -0800

    significant change and restructuring

--- aftershock_functions/get_prod_parameters.m
+++ aftershock_functions/get_prod_parameters.m
@@ -1,4 +1,15 @@
-function [prefactor,alpha,MAG, MAGCOUNT]=get_prod_parameters(t,lat,lon,depth,M)
+function [prefactor,alpha,MAG, MAGCOUNT, varargout]=get_prod_parameters(t,lat,lon,depth,M)
+% get parameter k (the prefactor), aplha (the slope - often 1), and the
+% average number of aftershock (MAGCOUNT) as a function of magnitude (MAG)
+% associtated to the aftershock productivity of earthquakes
+% given a catalog with time (t), location (lat, lon), depth and magnitude
+% (M)
+
+% varargout:
+%   varargout{1}: indexes of the mainshocks in the catalog
+%   varargout{2}: number of aftershocks of the earthquakes correspondly
+%                 indicated 
+
 
 x=lat;
 y=lon;
@@ -23,8 +34,8 @@ y=y(I);
 % Dtfor=-Dt;      % foreshock time window
 
 %% California Catalog
-Dmaxbig=100;        % max search radius for possible af's
-DistCutoff = 100;    % distance actually chosen      
+Dmaxbig=1000;        % max search radius for possible af's
+DistCutoff = 1000;    % distance actually chosen      
 Dtbig=3; % this is the time after the show;
 
 Dt=1;%(1000/60)/24; % time window in days
@@ -45,6 +56,10 @@ Rec(1).NumMain=[];
 Nmag=ceil((max(mag)-minmag)*10);
 Rec(Nmag)=Rec(1);
 maxmag=max(mag);
+
+mainShockIndex      = zeros(1,length(tsort));
+numberOfAftershock  = zeros(1,length(tsort));
+iMainShock = 0;
 
 for mm=maxmag:-0.1:minmag
     
@@ -81,14 +96,19 @@ for mm=maxmag:-0.1:minmag
     
     
             if (flag(I1(i))==0)
-                I             =(trel>0) &   (trel<Dt)   &   (D<DistCutoff);
+                IAS             =(trel>0) &   (trel<Dt)   &   (D<DistCutoff);
                 Ifor          =(trel<0)&(trel>Dtfor)&(D<10);
-                I(I1(i))      =0; % guard against including mainshock
+                IAS(I1(i))      =0; % guard against including mainshock
                 Ifor(I1(i))   =0;
                 %    if ((sum([flag(I) flag(Ifor)]))<1)   % none of the aftershocks or foreshocks are flagged
                 NumMain=NumMain+1;
-                Recm(i).DD=D(I);
+                Recm(i).DD=D(IAS);
                 Recm(i).Dfor=D(Ifor);
+                
+                % record the index of the mainshock in the original input array
+                iMainShock = iMainShock + 1;
+                mainShockIndex(iMainShock)      = I(I1(i)); % (I is the hash map for sorting and I1(i) is the main shock index in the sorted list of eq) 
+                numberOfAftershock(iMainShock) 	= sum(IAS);
             end
             
             
@@ -107,9 +127,14 @@ for mm=maxmag:-0.1:minmag
     
 end
 
+% trim outputs
 ijklast=ijk;
 Rec=Rec(1:ijklast);
+mainShockIndex      = mainShockIndex(1:iMainShock);
+numberOfAftershock  = numberOfAftershock(1:iMainShock);
 
+varargout{1} = mainShockIndex;
+varargout{2} = numberOfAftershock;
 
 % get the productivity: 
 % to do this I model the aftershock production (plotted aboves) as two

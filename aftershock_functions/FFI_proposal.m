cb8458faa13614c4e4df309996f0aa11c3ad4384
Kelian Dascher-Cousineau <kdascher@eduroam-169-233-226-185.ucsc.edu>
2019-02-27 05:35:00 -0800

    proposal figures

--- aftershock_functions/FFI_proposal.m
+++
@@ -1,6 +0,0 @@
-%% FINITE FAULT INVERSIONS ANALYSIS
-%% ML
-hayesCat = load('Hayes_fully_merged_catalog.mat');
-cat = hayesCat.cat;
-
-res = cat.MSres_appended_cat1;
---
+++ aftershock_functions/Plot_libraries/FFI_proposal.m
@@ -0,0 +1,6 @@
+%% FINITE FAULT INVERSIONS ANALYSIS
+%% ML
+hayesCat = load('Hayes_fully_merged_catalog.mat');
+cat = hayesCat.cat;
+
+res = cat.MSres_appended_cat1;
